47e3ef931730a570c9f60d28192eefa9233907db
SilviaIzmi <silviaizmi67@gmail.com>
2022-11-29 09:52:21 +0700

    first commit

--- app/src/main/java/com/example/tugas5_recycleview/DestinationActivity.java
+++ app/src/main/java/com/example/tugas5_recycleview/DestinationActivity.java
@@ -1,31 +1,96 @@
 package com.example.tugas5_recycleview;
 
-import android.os.Bundle;
-import android.view.View;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.work.ExistingWorkPolicy;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import com.example.tugas5_recycleview.databinding.ActivityDestinationBinding;
-
+import com.example.tugas5_recycleview.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class DestinationActivity extends AppCompatActivity {
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
     private ActivityDestinationBinding binding;
+    RecyclerView recylerView;
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.fartase,R.drawable.fresty,R.drawable.joly,R.drawable.scarlett};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding =
-                ActivityDestinationBinding.inflate(getLayoutInflater());
+        binding = ActivityDestinationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        final OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new
+                ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+        //action Bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav_view =
+                (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_message){
+                    Intent a = new
+                            Intent(DestinationActivity.this, MainActivity2.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_store){
+                    Intent a = new
+                            Intent(DestinationActivity.this, DestinationActivity.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_alarm){
+                    Intent a = new
+                            Intent(DestinationActivity.this, MainActivity.class);
+                    startActivity(a);
+                }
+                return true;
+            }
+        });
+        //recycle View
+        recylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.lotion);
+        s2 = getResources().getStringArray(R.array.star);
+        s3 = getResources().getStringArray(R.array.harga);
+        LotionAdapter appAdapter = new
+                LotionAdapter(this,s1,s2,s3,images);
+        recylerView.setAdapter(appAdapter);
+        LinearLayoutManager layoutManager = new
+                LinearLayoutManager(DestinationActivity.this,LinearLayoutManager
+                .HORIZONTAL, false
+        );
+        recylerView.setLayoutManager(layoutManager);
+        recylerView.setItemAnimator(new DefaultItemAnimator());
+        //work manager
+        final OneTimeWorkRequest request = new
+                OneTimeWorkRequest.Builder(MyWorker.class).build();
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WorkManager.getInstance().enqueueUniqueWork("Notifikasi", ExistingWorkPolicy.REPLACE, request);
+                WorkManager.getInstance().enqueueUniqueWork("Notifikasi",
+                        ExistingWorkPolicy.REPLACE, request);
             }
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        return abdt.onOptionsItemSelected(item) ||
+                super.onOptionsItemSelected(item);
     }
 }
